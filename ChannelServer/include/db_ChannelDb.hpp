5e6cfd437a20e4726ddfac13635a992761b77c09
Patrick Ching <burning.codex@gmail.com>
2022-06-07 14:51:26 -0600

    one-to-one header files

--- ChannelServer/include/db_ChannelDb.hpp
+++ ChannelServer/include/db_ChannelDb.hpp
@@ -2,14 +2,24 @@
 #include <string>
 #include <map>
 #include <time.h>
+#include "db_Account.hpp"
 
 namespace db
 {
 	using std::string;
 	using std::map;
 	
-	class ChannelDb
+	class ChannelDb /* : public AuraDb */
 	{
-		;
+		public:
+		ChannelDb();
+		~ChannelDb();
+
+		Account get_Account(string accountID);
+		
+
+		protected:
+
+		private:
 	};
 };
